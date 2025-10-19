0b85fc37e5269eec4e6c4aec64d9b7b7227f502b
Shova Moni <shovamoni0010@gmail.com>
2025-10-19 18:10:30 +0600

    Add files via upload
    Fetch Api (php)

---
+++ phpapi/api.php
@@ -0,0 +1,27 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "api_data");
+$response = array();
+
+if ($con) {
+    $sql = "SELECT * FROM data";
+    $result = mysqli_query($con,$sql);
+
+    if ($result) {
+        header("Content-Type: application/JSON");
+        $i = 0;
+        while ($row = mysqli_fetch_assoc($result)) {    
+            $response[$i]['name'] = $row['name'];
+            $response[$i]['email'] = $row['email'];
+            $response[$i]['id'] = $row['id'];
+            $response[$i]['salary'] = $row['salary'];
+            $response[$i]['date'] = $row['date'];
+            $i++;
+        }
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    } else {
+        echo "Query failed: " . mysqli_error($con);
+    }
+} else {
+    echo "Database connection failed: " . mysqli_connect_error();
+}
+?>
